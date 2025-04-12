3117c099b2a0033f3d24c1d02e3be912e4fd782a
Ariana Lopez <ariana.lopez.j@uni.pe>
2025-04-11 19:47:22 -0500

    Día 3: Implementación de la clase Quiz y flujo básico del juego

--- trivia.py
+++ trivia.py
@@ -6,3 +6,41 @@ class Question:
 
     def is_correct(self, answer):
         return self.correct_answer == answer
+
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question
+        return None
+    
+def run_quiz():
+    quiz = Quiz()
+
+    quiz.add_question(Question("¿Número de Departamentos del Perú?", ["24", "25", "26", "27"], "25"))
+
+    while True:
+        question = quiz.get_next_question()
+        if question is None:
+            print("¡Fin!")
+            break
+
+        print(f"\n{question.description}")
+        for idx, option in enumerate(question.options):
+            print(f"{idx + 1}. {option}")
+
+        answer = input("Tu respuesta: ")
+        selected_option = question.options[int(answer) - 1]
+
+        if question.is_correct(selected_option):
+            print("¡Correcto!")
+        else:
+            print(f"Incorrecto. La respuesta correcta es: {question.correct_answer}")
